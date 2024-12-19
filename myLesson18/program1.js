3c23f1505f11c31511036fa8e83850836daaaaf1
Nicole Kalcheva <nikol.kalcheva@gmail.com>
2024-12-19 11:26:18 +0200

    .

---
+++ myLesson18/program1.js
@@ -0,0 +1,5 @@
+
+let userName = prompt('Username required');
+let message = 'Hello!';
+
+console.log(message);
---
+++ myLesson18/program2.js
@@ -0,0 +1,4 @@
+const currentYear = 2024;
+let userBirthYear = prompt('Birth year requred');
+let result = currentYear - userBirthYear;
+console.log(`Your age is ${result}.`)
