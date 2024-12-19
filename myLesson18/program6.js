9858fa2671fd2cc67d646e585a19691b33bf2045
Nicole Kalcheva <nikol.kalcheva@gmail.com>
2024-12-19 11:43:16 +0200

    s

--- myLesson18/program6.js
+++ myLesson18/program6.js
@@ -2,4 +2,4 @@ const interstForYear = 5;
 let deposit= Number(prompt('Type your doposit for two months.'));
 let interestForTwoMonths = deposit * (interstForYear / 100) * (2 / 12);
 let finalSum = interestForTwoMonths + deposit;
-console.log(result.toFixed(2));
+console.log(finalSum.toFixed(2));
