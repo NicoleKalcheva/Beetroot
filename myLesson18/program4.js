0c522569d7d1fdde30930f29bda4f2992ab4caf4
Nicole Kalcheva <nikol.kalcheva@gmail.com>
2024-12-19 11:40:00 +0200

    .

--- myLesson18/program4.js
+++ myLesson18/program4.js
@@ -1,5 +1,5 @@
 let userMemory = prompt('Enter the memory of your flash drive in GB');
 let userMemoryConverted = userMemory*1000
 const memory = 820;
-let result = (userMemoryConverted / 820);
-console.log(Math.round(result));
+let howMany = (userMemoryConverted / 820);
+console.log(Math.round( howMany));
--- myLesson18/program6.js
+++ myLesson18/program6.js
@@ -1,5 +1,5 @@
 const interstForYear = 5;
 let deposit= Number(prompt('Type your doposit for two months.'));
 let interestForTwoMonths = deposit * (interstForYear / 100) * (2 / 12);
-let result = interestForTwoMonths + deposit;
+let finalSum = interestForTwoMonths + deposit;
 console.log(result.toFixed(2));
