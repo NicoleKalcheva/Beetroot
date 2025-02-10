615888ac415597fed4ce94a388836d76561ac1ea
Nicole Kalcheva <nikol.kalcheva@gmail.com>
2025-02-10 20:35:22 +0200

    .

---
+++ liveSesion/main.js
@@ -0,0 +1,10 @@
+const quotes = [
+    "Dream big. Work hard. Stay focused.",
+    "Your vibe attracts your tribe. ",
+    "Make today so awesome that yesterday gets jealous. ",
+    "Happiness looks good on you! ",
+    "Trust the timing of your life. "
+];
+function generator(){
+
+}
